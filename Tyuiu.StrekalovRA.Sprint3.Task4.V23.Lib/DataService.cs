1a76d143293a1dcab4417356171101aad183ca95
romikpistunov <romikpistunov@gmail.com>
2024-10-05 03:50:34 +0500

    task 4 only lib.

---
+++ Tyuiu.StrekalovRA.Sprint3.Task4.V23.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.StrekalovRA.Sprint3.Task4.V23.Lib
+{
+    public class DataService : ISprint3Task4V23
+    {
+        public double Calculate(int ST, int END)
+        {
+            double Res = 0;
+
+            for (int X = ST; X <= END; X += 1) {
+                if (X == 0) { continue; }
+                Res += ((Math.Cos(X) / X) + 3);
+            }
+            return Math.Round(Res, 3);
+        }
+    }
+}
